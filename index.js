491eb7a8d973c026cebfed73a968f8782557ed8b
momnhuong <songbao2903@gmail.com>
2022-05-22 17:55:41 +0700

    add index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Nhuong Trieu" }');
+    res.send('{ "response": "Hello From Bao Vy" }');
 });
 
 app.get('/will', function (req, res) {
