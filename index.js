24f6667b88374b102190f185bcbab9380cc6894a
momnhuong <songbao2903@gmail.com>
2022-05-23 11:50:17 +0700

    add index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Bao Trieu Ha Hy- Bao Trieu Ha Vy" }');
+    res.send('{ "response": "Hello From Test jenkins" }');
 });
 
 app.get('/will', function (req, res) {
