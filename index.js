8ac05d27a093661d21a6ab0281a72e4d179550a5
momnhuong <songbao2903@gmail.com>
2022-05-24 07:58:50 +0700

    L11

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Test Lần 10" }');
+    res.send('{ "response": "Hello From Test Lần 11" }');
 });
 
 app.get('/will', function (req, res) {
